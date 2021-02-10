1239f75e02b38dc13577a24efa423d38a3211b4a
Kevin L. Michel <78361257+KevinLMichel@users.noreply.github.com>
2021-02-10 17:35:24 -0500

    Update Actions.js

--- Actions.js
+++ Actions.js
@@ -3,3 +3,13 @@ var x = 10 + 5;
 document.write (x);
 var y = x + 5 + 22 + 45 + 6548;
 document.write(y);
+
+let hour = 14;
+if (hour >=6 && hour < 12) {
+document.write('Good morning welcome to the home of Adare Restaurant!');
+}
+else if (hour >=12 && hour < 18) {
+console.log('Good afternoon; and welcome to the home of Adare restaurant!');
+    }
+    else {console.log('Good evening! Adare Restaurant is closed, but yu can reserve a table for tomorrow.');
+    }
